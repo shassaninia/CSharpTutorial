c417b0d671bf7888437e1a66f7fd39761905dc33
SIMPLURIS\shassaninia <shassaninia@simpluris.com>
2017-03-07 14:49:39 -0500

    Print a message to console

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharpTutorial
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Welcome to C#");
     }
 }
+
