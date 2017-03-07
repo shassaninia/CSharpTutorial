e63b5740ef4b196e3c021283892ecc95d08fad2f
SIMPLURIS\shassaninia <shassaninia@simpluris.com>
2017-03-07 16:11:58 -0500

    static constructor

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -6,9 +6,15 @@ namespace CSharpTutorial
     //instance class
     class Circle
     {
-         static float _pi = 3.141f; // marking this static means that it will be shared across all instances.
+         static float _pi; // marking this static means that it will be shared across all instances.
                                     // _pi does not change on a per object basis so it makes sense to make it static
          int _radius;
+
+        static Circle()
+        {
+            //use static constructor to initialize static fields
+            _pi = 3.141f;
+        }
 
         public Circle(int radius)
         {
