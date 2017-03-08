31c4758e475791abb918fecd18d7b9a8bd2e3ebc
SIMPLURIS\shassaninia <shassaninia@simpluris.com>
2017-03-08 09:25:10 -0500

    The PrintFullName in the derived class PartTimeEmployee hides the PrintFulllName in the base class Employee.  If this intended add the new keyword

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -6,19 +6,9 @@ namespace CSharpTutorial
 
     class Employee
     {
-        public Employee()
-        {
-            Console.WriteLine("Base class constructor called");
-        }
 
-        public Employee(string message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
 
         public void PrintFullName()
         {
@@ -28,17 +18,19 @@ namespace CSharpTutorial
 
     class PartTimeEmployee : Employee
     {
-        public PartTimeEmployee() : base("Base class constructor with string parameter called")
-        {
-           
-        }
         public float HourlyRate { get; set; }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " - (Contractor)");
+        }
     }
 
     class FullTimeEmployee : Employee
     {
         public float YearlySalary { get; set; }
     }
+
     class Program
     {
         static void Main()
